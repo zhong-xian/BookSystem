8e2d17981eab14db4004fcbc207778e8ae9e34e9
zhong-xian <u0224048@mis.nkfust.edu.tw>
2015-06-22 22:17:32 +0800

    .

--- db.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-	 $dbServer ="127.0.0.1";
-	 $dbUser = "root";
-	 $dbPassword = "m940507";
-	 $dpname="booksystem";
-	 $conn = mysqli_connect($dbServer,$dbUser,$dbPassword,$dpname) or die("Error " . mysqli_error($link)); 
-	 mysqli_set_charset($conn,"utf8"); //---編碼問題----   重要!
-?>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-unset($_SESSION['name']);
-unset($_SESSION['loginFlog']);
-header('Location: login.html');
-
-?>
